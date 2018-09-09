bff15a8ad83100b193b60200b8c2d6deef8d9fea
James Halsall <james.halsall@inviqa.com>
2018-09-09 17:22:25 +0100

    Improve migrate command

--- src/Console/Command/MigrateCommand.php
+++ src/Console/Command/MigrateCommand.php
@@ -7,6 +7,7 @@ use Magento\Setup\Console\Command\DbStatusCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
@@ -17,11 +18,21 @@ use Symfony\Component\Process\Process;
  */
 class MigrateCommand extends Command
 {
+    private const MIGRATE_TIMEOUT = 900;
+    private const PHINX_BIN = 'vendor/robmorgan/phinx/bin/phinx';
+
     protected function configure()
     {
         $this
             ->setName('mx:db:migrate')
             ->setDescription('Migrates the database so it is ready for use with the current application code.');
+
+        $this->addOption(
+            'keep-generated',
+            null,
+            InputOption::VALUE_NONE,
+            'Prevents generated files from being deleted during migration.'
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -34,12 +45,21 @@ class MigrateCommand extends Command
         if ($exit === DbStatusCommand::EXIT_CODE_UPGRADE_REQUIRED) {
             $output->writeln('<info>A setup:upgrade is required, running...</info>');
             $setupUpgradeCommand = $this->getApplication()->get('setup:upgrade');
-            $setupUpgradeCommand->run(new ArrayInput([]), $output);
+
+            $setupUpgradeArgs = $input->getOption('keep-generated') ? ['--keep-generated' => true] : [];
+            $setupUpgradeCommand->run(new ArrayInput($setupUpgradeArgs), $output);
         } else {
             $output->writeln('<info>No setup:upgrade required.</info>');
         }
 
-        $phinx = new Process('bin/phinx migrate');
+        return $this->runPhinxMigrate();
+    }
+
+    protected function runPhinxMigrate(): int
+    {
+        $phinx = new Process(sprintf('%s migrate', self::PHINX_BIN));
+
+        $phinx->setTimeout(self::MIGRATE_TIMEOUT);
         $phinx->run(function ($type, $buffer) {
             echo $buffer;
         });
